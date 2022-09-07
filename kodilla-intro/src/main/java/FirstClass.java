32792f96de205704fee84295e223f66394ec237d
Adrian Swiatek <aswiatal@gmail.com>
2022-09-07 17:49:05 +0200

    First.Class java

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!"); // [3]
+    }
+}
+
