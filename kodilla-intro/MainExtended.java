f4aa832987c68a90f32e70a17696706922ca8e93
Adrian Swiatek <aswiatal@gmail.com>
2022-09-12 16:39:27 +0200

    First.Class java

---
+++ kodilla-intro/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+public static void main (String[] args) {
+    double height;
+    height=1.78
+    String Karol;
+    String myName=Karol
+    char grade='A';
+    System.out.println("On ma"+height+"wzrostu");
+    System.out.println("Wynik procentowy z egzaminu wyniośł"+height+"punktów");
+    System.out.println("Ściana na która się wspiął miała"+height+"wysokości");
+    System.out.println("Wczoraj na wykladzie poznałem"+myName);
+    System.out.println("Okazało się, że"+myName+"i ja jesteśmy identycznego wzrostu czyli dokładnie"+height);
+    System.out.println(myName+"otrzymał wynik z egzaminu, który wyniósł"+char)
+}
+}
---
+++ kodilla-intro/zmienne.java
@@ -0,0 +1,7 @@
+public class zmienne {
+}public class FirstClass {
+    public static void main(String[] args) {
+        String example = "Hello from FirstClass!";
+        System.out.println(myName);
+    }
+}
