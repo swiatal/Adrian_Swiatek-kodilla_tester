a7b6ea5f5dc6499faae47cfe07fcb4587d1b5ddf
Adrian Swiatek <aswiatal@gmail.com>
2022-09-28 22:30:45 +0200

    First.Class java

---
+++ kodilla-intro/src/RozbudowaInstrukcjiWarunkowej.java
@@ -0,0 +1,10 @@
+package src;
+
+public class RozbudowaInstrukcjiWarunkowej {if (yourAge < 10){
+    System.out.println("You're a kid");
+} else if (yourAge > 10 && yourAge < 18) {
+    System.out.println("You're a teenager");
+} else {
+    System.out.println("You're an adult");
+}
+}
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2000;
+        if (year % 4 == 0) {
+            System.out.println("przestępny");
+        } else if (year % 100 == 0) {
+            System.out.println("przestępny");
+        } else if (year % 400 == 0) {
+            System.out.println("przestepny");
+        } else {
+            System.out.println("nie przestępny");
+        }
+    }
+}
+
