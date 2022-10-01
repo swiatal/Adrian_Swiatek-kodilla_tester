8d7730be9cfe956332dd4be61d14b7438cea355e
Adrian Swiatek <aswiatal@gmail.com>
2022-10-01 20:57:13 +0200

    First.Class java

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,6 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+     String[] headphones = new String[]{"Susvara", "arya", "LCD-X", "Meze Elite", "Abyss diana v2"};
+     int numberOfElements = headphones.length;
+    }
+}
