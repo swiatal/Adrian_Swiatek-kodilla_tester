d2c5b9beb50fe9b4df175f6c9f040e69a9b01b31
Adrian Swiatek <aswiatal@gmail.com>
2022-10-02 14:08:16 +0200

    First.Class java

--- kodilla-intro/src/main/java/SimpleArray.java
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -1,6 +1,7 @@
 public class SimpleArray {
     public static void main(String[] args) {
      String[] headphones = new String[]{"Susvara", "arya", "LCD-X", "Meze Elite", "Abyss diana v2"};
+     System.out.println("Moja tablica zawiera" + headphones.length + "elementów");
      int numberOfElements = headphones.length;
     }
 }
