fcf4a59aecf7bad36a031bee92d940437d8b0bae
Adrian Swiatek <aswiatal@gmail.com>
2022-10-27 16:47:54 +0200

    First.Class java

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -5,18 +5,29 @@ public class Person {
     private int age;
     private Job job;
 
-    public Person(String firstName,int age, Job job){
+    public Person(String firstName, int age, Job job) {
         this.firstName = firstName;
         this.age = age;
         this.job = job;
     }
-   public static void main(String[]args) {
 
-   Job teacher = new Teacher (3000,"giving homeworks");
-   teacher.responsibilitiesInWork();
+    public void showResponsibilities(String ResponsibilitiesInWork) {
+       System.out.println(ResponsibilitiesInWork + "firstName" + "age" + "job");
 
-   Job accountant = new Accountant(5000, "preparation of the profit and loss account"
-   );
-   accountant.responsibilitiesInWork();
+        }
+        public static void main (String[]args){
+
+            Job teacher = new Teacher(3000, "giving homeworks");
+            teacher.responsibilitiesInWork();
+Teacher.showResponsibilities("Teacher=");
+
+            Job accountant = new Accountant(5000, "preparation of the profit and loss account"
+            );
+            accountant.responsibilitiesInWork();
+        }
     }
-}
+
+
+
+
+
