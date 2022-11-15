67e8691f0791b55842f9939325937436ff13bb7a
Adrian Swiatek <aswiatal@gmail.com>
2022-11-15 19:19:46 +0100

    First.Class java

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutbale/homework/ImmutableExercise.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutbale/homework/ImmutableExercise.java
@@ -1,8 +1,11 @@
 package com.kodilla.collections.adv.immutbale.homework;
 
 public class ImmutableExercise {
+
     public static void main(String[] args) {
         Task task = new Task("Idiot", '6');
         TaskRecord taskRecord = new TaskRecord("Demons", '9');
     }
+
 }
+
