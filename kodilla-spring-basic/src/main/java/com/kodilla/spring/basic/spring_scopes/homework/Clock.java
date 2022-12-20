0de6404cd624586b7287358113e427388ecf8e86
Adrian Swiatek <aswiatal@gmail.com>
2022-12-20 12:26:05 +0100

    First.Class java

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -3,13 +3,20 @@ package com.kodilla.spring.basic.spring_scopes.homework;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Component
 @Scope("prototype")
 public class Clock {
-    public LocalTime time = LocalTime.now();
-    public LocalTime getTime(){
-        return time;
+    private LocalTime localTime;
+
+
+    public void updateTime() {
+        localTime = LocalTime.now();
     }
-}
+
+       public LocalTime getLocalTime() {
+        return localTime;
+        }
+    }
--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTest.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTest.java
@@ -1,28 +1,28 @@
 package com.kodilla.spring.basic.spring_scopes.homework;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.time.LocalTime;
-@SpringBootTest
+
 public class ClockTest {
+    private ApplicationContext context;
+
+    @BeforeEach
+    public void setUpContext() {
+        context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+    }
     @Test
     public void shouldNotSameTime() {
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Clock firstBean = context.getBean(Clock.class);
-        Clock secondBean = context.getBean(Clock.class);
-        Clock thirdBean = context.getBean(Clock.class);
-        //when
-        LocalTime firstTime = firstBean.getTime();
-        LocalTime secondTime = secondBean.getTime();
-        LocalTime thirdTime = thirdBean.getTime();
+        Clock clock = context.getBean(Clock.class);
+        clock.updateTime();
+        Clock secondClock = context.getBean(Clock.class);
+        secondClock.updateTime();
 
-        //then
-        Assertions.assertNotEquals(firstTime, secondTime);
-        Assertions.assertNotEquals(secondTime, thirdTime);
-        Assertions.assertNotEquals(firstTime, thirdTime);
+        Assertions.assertNotEquals(clock.getLocalTime(),secondClock.getLocalTime());
     }
 }
