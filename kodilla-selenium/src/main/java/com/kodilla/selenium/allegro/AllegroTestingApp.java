6d51fa5880f4a51242616ae9ecdb67091f5dd30e
swiatal <aswiatal@gmail.com>
2023-01-06 22:07:49 +0100

    First.Class java

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,27 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.allegro.pl/");
+
+        WebDriverWait wait = new WebDriverWait (driver, 10);
+
+        WebElement category = driver.findElement(By.xpath("//*/div[@class=\"mp7g_oh mr3m_1 s4kyg\"]/select/optgroup/option[3]"));
+        wait.until(ExpectedConditions.visibilityOf(category)).click();
+
+        WebElement inputField = driver.findElement(By.xpath("//*/input[@name=\"string\"]"));
+        wait.until(ExpectedConditions.visibilityOf(inputField));
+        inputField.sendKeys("Mavic mini");
+        inputField.submit();
+    }
+}
