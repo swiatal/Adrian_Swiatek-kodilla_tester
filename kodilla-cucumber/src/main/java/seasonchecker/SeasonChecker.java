cd7e6c252c0a90838b4554bf1be6ff8b6b4fda6a
swiatal <aswiatal@gmail.com>
2023-01-18 21:04:15 +0100

    First.Class java

--- kodilla-cucumber/src/main/java/seasonchecker/SeasonChecker.java
+++ kodilla-cucumber/src/main/java/seasonchecker/SeasonChecker.java
@@ -3,7 +3,10 @@ package seasonchecker;
 import java.time.MonthDay;
 
 public class SeasonChecker {
-    public static String checkIfSummer(MonthDay date) {
+    public String checkIfSummer(MonthDay date) {
+        if (date.isAfter(MonthDay.of(6,20)) && date.isBefore(MonthDay.of(9, 24))) {
+            return "Yes! Summer!";
+        }
         return "Nope";
     }
 }
